582eed5607c4a6a25cd1fcbcf6399340afb97763
lauriehillier <laurie.hillier@gmail.com>
2024-01-15 15:06:17 +0000

    complete task 04: get article by id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -27,12 +27,10 @@ describe("/api", () => {
           Object.keys(endpoints).forEach((endpointKey) => {
             expect(typeof endpoints[endpointKey].description).toBe("string");
             expect(Array.isArray(endpoints[endpointKey].queries)).toBe(true);
-            expect(typeof endpoints[endpointKey].exampleRequest).toBe(
+            expect(typeof endpoints[endpointKey].exampleRequest).toBe("object");
+            expect(typeof endpoints[endpointKey].exampleResponse).toBe(
               "object"
             );
-            expect(typeof endpoints[endpointKey].exampleResponse).toBe(
-                "object"
-              );
           });
         });
     });
@@ -53,5 +51,42 @@ describe("/api", () => {
       });
     });
   });
+  describe("/articles/:article_id", () => {
+    describe("GET", () => {
+      test("200: sends an object of the article with the given id", () => {
+        return request(app)
+          .get("/api/articles/5")
+          .expect(200)
+          .then(({ body }) => {
+            expect(body.article).toEqual({
+              article_id: 5,
+              title: "UNCOVERED: catspiracy to bring down democracy",
+              topic: "cats",
+              author: "rogersop",
+              body: "Bastet walks amongst us, and the cats are taking arms!",
+              created_at: "2020-08-03T13:14:00.000Z",
+              votes: 0,
+              article_img_url:
+                "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+            });
+          });
+      });
+      test("400: sends an appropriate error if id is invalid (i.e. a string)", () => {
+        return request(app)
+          .get("/api/articles/hello")
+          .expect(400)
+          .then(({ body }) => {
+            expect(body.msg).toBe("Bad Request");
+          });
+      });
+      test("404: sends an appropriate error if id is valid but doesn't exist", () => {
+        return request(app)
+          .get("/api/articles/744859587")
+          .expect(404)
+          .then(({ body }) => {
+            expect(body.msg).toBe("Article not found");
+          });
+      });
+    });
+  });
 });
-// test for bad path?
