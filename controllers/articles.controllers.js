a0014077055e0aa54cfd71600d680655747c293d
lauriehillier <laurie.hillier@gmail.com>
2024-01-15 16:16:29 +0000

    complete task 05: get articles

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,9 +1,21 @@
-const { selectSingleArticle } = require("../models/articles.models");
+const {
+  selectSingleArticle,
+  selectArticles,
+} = require("../models/articles.models");
 
 exports.getSingleArticle = async (req, res, next) => {
   try {
     const article = await selectSingleArticle(req.params.article_id);
     res.status(200).send({ article });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getArticles = async (req, res, next) => {
+  try {
+    const articles = await selectArticles();
+    res.status(200).send({ articles });
   } catch (err) {
     next(err);
   }
