8a80096dd9a7d48d2d3d0a3140a4e27c9a878a78
lauriehillier <laurie.hillier@gmail.com>
2024-01-16 02:16:02 +0000

    complete task 09: delete commment by id

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const {
 const {
   getCommentsByArticle,
   postCommentByArticle,
+  deleteCommentById,
 } = require("./controllers/comments.controllers");
 const {
   handleCustomErrors,
@@ -26,6 +27,7 @@ app.get("/api/articles/:article_id", getSingleArticle);
 app.patch("/api/articles/:article_id", patchSingleArticle);
 app.get("/api/articles/:article_id/comments", getCommentsByArticle);
 app.post("/api/articles/:article_id/comments", postCommentByArticle);
+app.delete("/api/comments/:comment_id", deleteCommentById);
 app.all("/*", badPath);
 
 // Error Handling
--- models/comments.models.js
+++ models/comments.models.js
@@ -27,3 +27,11 @@ exports.insertCommentByArticle = async (article_id, newComment) => {
   );
   return rows[0];
 };
+
+exports.removeCommentById = async (comment_id) => {
+  const { rows } = await db.query(
+    "DELETE FROM comments WHERE comment_id = $1 RETURNING *",
+    [comment_id]
+  );
+  if (!rows.length) return Promise.reject({ status: 404, msg: "Comment not found" })
+};
