75b3c07c72aabed72bd432a45b6215ea65cc4325
lauriehillier <laurie.hillier@gmail.com>
2024-01-17 10:57:12 +0000

    complete task 16: routers

--- app.js
+++ app.js
@@ -1,35 +1,18 @@
 const express = require("express");
-const { getTopics } = require("./controllers/topics.controllers");
-const { badPath, getEndpoints } = require("./controllers/app.controllers");
-const {
-  getSingleArticle,
-  getArticles,
-  patchSingleArticle,
-} = require("./controllers/articles.controllers");
-const {
-  getCommentsByArticle,
-  postCommentByArticle,
-  deleteCommentById,
-} = require("./controllers/comments.controllers");
+const { badPath } = require("./controllers/app.controllers");
 const {
   handleCustomErrors,
   handlePsqlErrors,
   handleServerErrors,
 } = require("./errors");
-const { getUsers } = require("./controllers/users.controllers");
+const apiRouter = require("./routes/api-router");
+
+// app setup
 const app = express();
 app.use(express.json());
 
-// End Points
-app.get("/api", getEndpoints);
-app.get("/api/topics", getTopics);
-app.get("/api/articles", getArticles);
-app.get("/api/articles/:article_id", getSingleArticle);
-app.patch("/api/articles/:article_id", patchSingleArticle);
-app.get("/api/articles/:article_id/comments", getCommentsByArticle);
-app.post("/api/articles/:article_id/comments", postCommentByArticle);
-app.delete("/api/comments/:comment_id", deleteCommentById);
-app.get("/api/users", getUsers);
+// Endpoints
+app.use('/api', apiRouter);
 app.all("/*", badPath);
 
 // Error Handling
---
+++ routes/topics-router.js
@@ -0,0 +1,6 @@
+const { getTopics } = require('../controllers/topics.controllers');
+const topicRouter = require('express').Router();
+
+topicRouter.get("/", getTopics);
+
+module.exports = topicRouter;
